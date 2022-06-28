f9f16322e97e97ca5e8b4005ae95f71eb0d6af9c
lital hazot <lital7h@gmail.com>
2022-06-28 16:46:58 +0300

    first commit

---
+++ CalcApi/Controllers/MathController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CalcApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+
+        [HttpPost("add")]
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        [HttpPost("substract")]
+        public int Substract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        [HttpPost("multiply")]
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+        [HttpPost("divide")]
+        public int Divide(int value1, int value2)
+        {
+            if (value2 == 0)
+            {
+                return 0;
+            }
+
+            else
+            {
+                return value1 / value2;
+            }
+        }
+
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    return "default";
+        //}
+    }
+}
+
---
+++ CalcApi/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CalcApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Implement Swagger calc",
+                    Description = "My swagger calc",
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Showing API V1");
+                });
+
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
